a3f21364f73045f657724f0d2047ae313e9b781b
justbraf <dwaynecorey+github@gmail.com>
2021-12-10 11:18:00 -0400

    read and display basic api data

--- js/data.js
+++ js/data.js
@@ -3,8 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 let grabData = (url) => {
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => appendData(data))
     .catch(err => console.log(err));
+}
+
+let appendData = (data) => {
+    // console.log(data);
+    let myDiv = document.getElementById("pokedata"); // create reference to element on webpage
+    let newDiv = document.createElement("div"); // create new element from scratch
+    newDiv.innerHTML = "<p>Name:&nbsp;" + data.name + "</p>"; // add content to element
+    newDiv.innerHTML += "<p>Abilities:</p><ul>";
+    for (eachAbility of data.abilities)
+        newDiv.innerHTML += "<li>" + eachAbility.ability.name + "</li>";
+    newDiv.innerHTML += "</ul>";
+    myDiv.appendChild(newDiv);
 }
