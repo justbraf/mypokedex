872dd79fc180fb996f137694855794d953c2bd4e
justbraf <dwaynecorey+github@gmail.com>
2021-12-10 10:42:57 -0400

    created a promise to read the api

--- js/data.js
+++ js/data.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     alert("hi");
 });
+
+let grabData = (url) => {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then(response => response.json())
+    .then(data => console.table(data))
+    .catch(err => console.log(err));
+}
