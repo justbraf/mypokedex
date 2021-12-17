eadf47891d2640fe2200d9b097d7a1f163f37254
justbraf <dwaynecorey+github@gmail.com>
2021-12-17 14:04:54 -0400

    updated UI; implemented nav; added search;

--- js/data.js
+++ js/data.js
@@ -1,28 +1,90 @@
 document.addEventListener("DOMContentLoaded", () => {
-    grabData("https://pokeapi.co/api/v2/pokemon/3");
+    grabData("https://pokeapi.co/api/v2/pokemon/1", "load");
+    grabData("https://pokeapi.co/api/v2/pokemon?limit=5", "navi");
+    // let pokelist = document.querySelector("#pokelist");
+    // pokelist.addEventListener("change", event => grabData(pokelist.options[pokelist.selectedIndex].value, "load"));
 });
 
 // function to fetch/read data using a JavaScript promise
-let grabData = (url) => {
+let grabData = (url, mode) => {
     fetch(url)
     .then(response => response.json()) // convert promise data to json
-    .then(data => appendData(data)) // take returned json data and pass it to a function
+    .then(data => {
+        if (mode == "load")
+            appendData(data);
+        else
+            appendNavigation(data);
+    }) // take returned json data and pass it to a function
     .catch(err => console.log(err));
 }
 
 // appendData JSON data to webpage
 let appendData = (data) => {
     // console.log(data.sprites);
-    let myDiv = document.getElementById("pokedata"); // create reference to element on webpage
-    let newDiv = document.createElement("div"); // create new element from scratch
+    let pokeData = document.querySelector("#pokedata"); // create reference to element on webpage
+    let pokeImg = document.querySelector("#pokeimg"); // create reference to element on webpage
+    
     let unorderedList = document.createElement("ul"); // create an unordered list
     let li;
-    newDiv.innerHTML = "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + data.id + ".png'/><p>Name:&nbsp;" + data.name + "</p><p>Abilities:</p>"; // add html to a div element
+
+    pokeImg.innerHTML = "<img id='pokeImg' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + data.id + ".png'/>"; // add html to a div element
+    
+    const pokeName = data.name;
+    pokeData.innerHTML = "<p>Name:&nbsp;" + capitalizeWord(data.name) + "</p><p>Abilities:</p>";
     for (eachAbility of data.abilities) {
         li = document.createElement("li");
-        li.appendChild(document.createTextNode(eachAbility.ability.name));
+        li.appendChild(document.createTextNode(capitalizeWord(eachAbility.ability.name)));
         unorderedList.appendChild(li);
     }
-    newDiv.appendChild(unorderedList);
-    myDiv.appendChild(newDiv);
+
+    pokeData.appendChild(unorderedList);
 }
+
+// creates navigation pane
+let appendNavigation = (data) => {
+    let parentPrev = document.querySelector("#prev");
+    let parentNext = document.querySelector("#next");
+    let pokelist = document.querySelector("#pokelist");
+
+    parentPrev.innerHTML = "";
+    parentNext.innerHTML = "";
+
+    while (pokelist.length > 0)
+        pokelist.remove(0);
+    let opts;
+    for (pokemon of data.results) {
+        opts = document.createElement("option");
+        opts.text = capitalizeWord(pokemon.name);
+        opts.value = pokemon.url;
+        pokelist.add(opts, null);
+    }
+    pokelist.addEventListener("change", event => grabData(pokelist.options[pokelist.selectedIndex].value, "load"));
+    let defPokemon = pokelist.options[0].value;
+
+    if (data.previous != null) {
+        let prevBtn = document.createElement("a");
+        prevBtn.innerHTML = "Previous";
+        prevBtn.href = data.previous;
+        prevBtn.classList.add("btn");
+        prevBtn.classList.add("btn-info");
+        parentPrev.appendChild(prevBtn);
+    }
+
+    if (data.next != null) {
+        let nextBtn = document.createElement("a");
+        nextBtn.innerHTML = "Next";
+        nextBtn.href = data.next;
+        nextBtn.classList.add("btn");
+        nextBtn.classList.add("btn-info");
+        // nextBtn.setAttribute("id", "nextLink");
+        nextBtn.addEventListener("click", event => {
+            event.preventDefault(); // prevents normal behaviour
+            grabData(event.target.href, "navi");
+            grabData(defPokemon, "load");
+        });
+        parentNext.appendChild(nextBtn);
+    }
+}
+
+// Capitalize the first letter in a word
+let capitalizeWord = (word) => word.charAt(0).toUpperCase() + word.slice(1);
